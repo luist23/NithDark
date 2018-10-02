f9b19f2ec86163657be2f6d186195d5285558ed8
luist23 <00127717@uca.edu.sv>
2018-10-01 17:53:18 -0700

    v:

---
+++ Tareas/porJoder/juder.js
@@ -0,0 +1,13 @@
+
+
+var rombo=function rombo(n){
+    for (let i=1;i<n;i++){
+        console.log('*'.repeat(i));
+    }
+    for (let i=n;i>=1;i--){
+        console.log('*'.repeat(i));
+    }
+    
+}
+
+document.getElementsByTagNameNS
